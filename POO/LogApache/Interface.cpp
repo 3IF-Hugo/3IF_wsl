5178a23d3be22ee0fd7c55ba8ee66583e70f4f73
Grel Hugo <grelhugo@gmail.com>
2022-01-27 11:46:24 +0100

    Interface

--- POO/LogApache/Interface.cpp
+++ POO/LogApache/Interface.cpp
@@ -38,71 +38,89 @@ int main(int argc, char * argv[])
     int gPresent = chercherOtions(args, g);
     int ePresent = chercherOtions(args, e);
     int tPresent = chercherOtions(args, t);
+    try
+    {
+        if(gPresent != 0 && (gPresent < ePresent || gPresent < tPresent))
+        {
+            throw string("L'option -g doit être mise en dernier ! Relancer l'application...");
+        }
+        if(argc == 2)
+        {
+            ungraphe->LectureFichier(args[argc-1], 0, 0, -1);
+            ungraphe->FaireTop10();
+            cout << "Top 10 : " << endl;
+            ungraphe->AfficherTop10();
+        }else if(tPresent != 0 && gPresent != 0 && ePresent != 0 && argc == 7)
+        {
+            int heure = stoi(args[tPresent+1]);
 
-    if(argc == 2)
-    {
-        ungraphe->LectureFichier(args[argc-1], 0, 0, -1);
-        ungraphe->FaireTop10();
-        cout << "Top 10 : " << endl;
-        ungraphe->AfficherTop10();
-    }else if(tPresent != 0 && gPresent != 0 && ePresent != 0)
-    {
-        int heure = stoi(args[tPresent+1]);
+            ungraphe->LectureFichier(args[argc-1], 1, 1, heure);
+            ungraphe->Generer(args[gPresent+1]);
+            cout << "Dot-file "<< args[gPresent+1] << " generated" << endl;
+            cout << "Il ne contient pas les extensions css, image, javascript" << endl;
+            cout << "WARNING : only hits between " << heure << " and " << heure+1 << " have been taken into account" << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }else if((gPresent != 0) && (ePresent != 0) && argc == 5)
+        {
+            ungraphe->LectureFichier(args[argc-1], 1, 1, -1);
+            ungraphe->Generer(args[gPresent+1]);
+            cout << "Dot-file "<< args[gPresent+1] << " generated" << endl;
+            cout << "Il ne contient pas les extensions css, image, javascript" << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }else if(tPresent != 0 && gPresent != 0 && argc == 6)
+        {
+            int heure = stoi(args[tPresent+1]);
 
-        ungraphe->LectureFichier(args[argc-1], 1, 1, heure);
-        ungraphe->Generer(args[gPresent+1]);
-        cout << "Dot-file "<< args[gPresent+1] << " generated" << endl;
-        cout << "Il ne contient pas les extensions css, image, javascript" << endl;
-        cout << "WARNING : only hits between " << heure << " and " << heure+1 << " have been taken into account" << endl;
-        ungraphe->FaireTop10();
-        ungraphe->AfficherTop10();
-    }else if((gPresent != 0) && (ePresent != 0))
-    {
-        ungraphe->LectureFichier(args[argc-1], 1, 1, -1);
-        ungraphe->Generer(args[gPresent+1]);
-        cout << "Dot-file court.dot generated" << endl;
-        cout << "Il ne contient pas les extensions css, image, javascript" << endl;
-        ungraphe->FaireTop10();
-        ungraphe->AfficherTop10();
-    }else if(tPresent != 0 && gPresent != 0)
-    {
-        int heure = stoi(args[tPresent+1]);
+            ungraphe->LectureFichier(args[argc-1], 1, 0, heure);
+            ungraphe->Generer(args[gPresent+1]);
+            cout << "Dot-file "<< args[gPresent+1] << " generated" << endl;
+            cout << "Il ne contient pas les extensions css, image, javascript" << endl;
+            cout << "WARNING : only hits between " << heure << " and " << heure+1 << " have been taken into account" << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }else if(gPresent != 0 && argc == 4)
+        {
+            cout << "je passe par la" << endl;
+            cout << args[argc-1] << endl;
+            ungraphe->LectureFichier(args[argc-1], 1, 0, -1);
+            //ungraphe->LectureFichier("fichierInput.txt", 1, 0, -1);
+            ungraphe->Generer(args[gPresent+1]);
+            cout << "Dot-file "<< args[gPresent+1] << " generated" << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }else if(tPresent != 0 && ePresent != 0 && argc == 5)
+        {
+            int heure = stoi(args[tPresent+1]);
+            ungraphe->LectureFichier(args[argc-1], 0, 1, heure);
+            cout << "Top 10 dans un créneau horaire sans les extensions : " << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }else if(ePresent != 0 && argc == 3)
+        {
+            ungraphe->LectureFichier(args[argc-1], 0, 1, -1);
+            cout << "Top 10 sans extensions : " << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }else if(tPresent != 0 && argc == 4)
+        {
+            int heure = stoi(args[tPresent+1]);
 
-        ungraphe->LectureFichier(args[argc-1], 1, 0, heure);
-        ungraphe->Generer(args[gPresent+1]);
-        cout << "Dot-file court.dot generated" << endl;
-        cout << "Il ne contient pas les extensions css, image, javascript" << endl;
-        cout << "WARNING : only hits between " << heure << " and " << heure+1 << " have been taken into account" << endl;
-        ungraphe->FaireTop10();
-        ungraphe->AfficherTop10();
-    }else if(gPresent != 0)
-    {
-        cout << "je passe par la" << endl;
-        cout << args[argc-1] << endl;
-        ungraphe->LectureFichier(args[argc-1], 1, 0, -1);
-        //ungraphe->LectureFichier("fichierInput.txt", 1, 0, -1);
-        ungraphe->Generer(args[gPresent+1]);
-        cout << "Dot-file court.dot generated" << endl;
-        ungraphe->FaireTop10();
-        ungraphe->AfficherTop10();
-    }else if(ePresent != 0)
-    {
-        ungraphe->LectureFichier(args[argc-1], 0, 1, -1);
-        cout << "Top 10 sans extensions : " << endl;
-        ungraphe->FaireTop10();
-        ungraphe->AfficherTop10();
-    }else if(tPresent != 0)
-    {
-        int heure = stoi(args[tPresent+1]);
-
-        ungraphe->LectureFichier(args[argc-1], 0, 0, heure);
-        cout << "Top 10 dans un créneau horaire : " << endl;
-        ungraphe->FaireTop10();
-        ungraphe->AfficherTop10();
+            ungraphe->LectureFichier(args[argc-1], 0, 0, heure);
+            cout << "Top 10 dans un créneau horaire : " << endl;
+            ungraphe->FaireTop10();
+            ungraphe->AfficherTop10();
+        }
+        else{
+            throw string("Options invalides ! Relancer l'application...");
+            // cerr << "Options invalides" << endl;
+            // cerr << "Fermeture de l'application" << endl;
+        }
     }
-    else{
-        cerr << "Options invalides" << endl;
-        cerr << "Fermeture de l'application" << endl;
+    catch(string const& chaine)
+    {
+        cerr << chaine << endl;
     }
 
     return 0;
